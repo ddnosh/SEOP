7794411125505a138c561494f04b598399f6e5e4
ddnosh <cage.hung@gmail.com>
2018-12-16 13:41:42 +0800

    引入treeview第三方库

--- app/src/main/java/com/androidwind/seop/MainActivity.java
+++ app/src/main/java/com/androidwind/seop/MainActivity.java
@@ -1,13 +1,49 @@
 package com.androidwind.seop;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.unnamed.b.atv.holder.IconTreeItemHolder;
+import com.unnamed.b.atv.holder.SelectableHeaderHolder;
+import com.unnamed.b.atv.model.TreeNode;
+import com.unnamed.b.atv.view.AndroidTreeView;
+
+public class MainActivity extends AppCompatActivity implements TreeNode.TreeNodeClickListener{
+
+    private AndroidTreeView tView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewGroup containerView = (ViewGroup) findViewById(R.id.container);
+        TreeNode root = TreeNode.root();
+        TreeNode ui = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_folder, "UI")).setViewHolder(
+                new SelectableHeaderHolder(this));
+        TreeNode dialog = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_folder, "Dialog")).setViewHolder(new SelectableHeaderHolder(this));
+        TreeNode sheet = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_folder, "Sheet")).setViewHolder(new SelectableHeaderHolder(this));
+
+        ui.addChildren(dialog, sheet);
+
+        TreeNode architecture = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_folder, "ARCHITECTURE")).setViewHolder(
+                new SelectableHeaderHolder(this));
+        root.addChildren(ui, architecture);
+
+        tView = new AndroidTreeView(this, root);
+        tView.setDefaultAnimation(true);
+        tView.setUse2dScroll(true);
+        tView.setDefaultContainerStyle(R.style.TreeNodeStyleCustom);
+        tView.setDefaultNodeClickListener(this);
+        containerView.addView(tView.getView());
+
+//        tView.expandAll();
+    }
+
+    @Override
+    public void onClick(TreeNode node, Object value) {
+        Toast toast = Toast.makeText(this, ((IconTreeItemHolder.IconTreeItem)value).text, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
