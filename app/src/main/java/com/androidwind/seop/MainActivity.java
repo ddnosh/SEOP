7421646478ba3bd7b4a6568e92a286084a136b2b
ddnosh <cage.hung@gmail.com>
2018-12-31 21:03:41 +0800

    增加目录列表初始化功能

--- app/src/main/java/com/androidwind/seop/MainActivity.java
+++ app/src/main/java/com/androidwind/seop/MainActivity.java
@@ -6,56 +6,85 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.ViewGroup;
 
+import com.androidwind.seop.bean.MenuBean;
 import com.androidwind.seop.ui.refresh.SmartRefreshLayoutActivity;
+import com.androidwind.seop.util.MenuUtil;
 import com.unnamed.b.atv.holder.IconTreeItemHolder;
 import com.unnamed.b.atv.holder.SelectableHeaderHolder;
 import com.unnamed.b.atv.model.TreeNode;
 import com.unnamed.b.atv.view.AndroidTreeView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements TreeNode.TreeNodeClickListener {
 
+    private TreeNode root;
     private AndroidTreeView tView;
+    private List<MenuBean> menuList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ViewGroup containerView = (ViewGroup) findViewById(R.id.container);
-        TreeNode root = TreeNode.root();
-        //ui
-        TreeNode ui = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_folder, "UI")).setViewHolder(
-                new SelectableHeaderHolder(this));
-        //ui -> refresh
-        TreeNode refresh = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_folder, "Refresh")).setViewHolder(new SelectableHeaderHolder(this));
-        //ui -> refresh -> smartrefreshlayout
-        TreeNode smartRefreshLayout = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_place, "SmartRefreshLayout")).setViewHolder(new SelectableHeaderHolder(this));
-        // add smartRefreshLayout children
-        refresh.addChild(smartRefreshLayout);
-        //add ui children
-        ui.addChildren(refresh);
+        ViewGroup containerView = findViewById(R.id.container);
 
-        TreeNode architecture = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_folder, "ARCHITECTURE")).setViewHolder(
-                new SelectableHeaderHolder(this));
+        root = TreeNode.root();
+        menuList = MenuUtil.getPositions(this, "menu.txt");
+        initMenus();
 
-        //add root children
-        root.addChildren(ui, architecture);
-
+        //init AndroidTreeView
         tView = new AndroidTreeView(this, root);
         tView.setDefaultAnimation(true);
         tView.setUse2dScroll(true);
         tView.setDefaultContainerStyle(R.style.TreeNodeStyleCustom);
         tView.setDefaultNodeClickListener(this);
         containerView.addView(tView.getView());
+    }
 
-//        tView.expandAll();
+    private void initMenus() {
+        for (MenuBean bean : menuList) {
+            if (bean.treeNode == null) {
+                getTreeNode(bean);
+            }
+        }
     }
 
     @Override
     public void onClick(TreeNode node, Object value) {
         String name = ((IconTreeItemHolder.IconTreeItem) value).text;
         if (!TextUtils.isEmpty(name)) {
-            if (name.equals("SmartRefreshLayout"))
+            if (name.equals("SmartRefreshLayout")) {
                 startActivity(new Intent(this, SmartRefreshLayoutActivity.class));
+            }
         }
+    }
+
+    private TreeNode getTreeNode(MenuBean menuBean) {
+        //root
+        if (menuBean.upperId == 0) {
+            if (menuBean.treeNode == null) {
+                return menuBean.treeNode = addTreeNode(root, menuBean.name);
+            }
+        } else {
+            for (MenuBean bean : menuList) {
+                if (bean.currentId == menuBean.upperId) {
+                    if (bean.treeNode == null) {
+                        return menuBean.treeNode = getTreeNode(menuBean);
+                    } else {
+                        return menuBean.treeNode = addTreeNode(bean.treeNode, menuBean.name);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+
+    private TreeNode addTreeNode(TreeNode treeNode, String nodeName) {
+        if (treeNode == null) return null;
+        TreeNode node = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_folder, nodeName)).setViewHolder(
+                new SelectableHeaderHolder(this));
+        treeNode.addChild(node);
+        return node;
     }
 }
